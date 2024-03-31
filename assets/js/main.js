5d46d991f079a0aaed41e3ff066f9b4faf909185
nwebpro <abnaeem.bsc@gmail.com>
2024-03-31 11:46:04 +0600

    about page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/*--------------------------
-    Project Name: Promina
-    Version: 1.0
-    Author: 7oorof
-    Devloped by: Ahmed Abdallah (a.abdallah999@gmail.com)
-    Relase Date: June 2020
----------------------------*/
 /*---------------------------
       Table of Contents
     --------------------
@@ -26,209 +19,198 @@
  ----------------------------*/
 
 $(function () {
+	'use strict';
 
-    "use strict";
+	// Global variables
+	var $win = $(window);
 
-    // Global variables
-    var $win = $(window);
+	/*==========  Pre Loading   ==========*/
+	setTimeout(function () {
+		$('.preloader').remove();
+	}, 2000);
 
-    /*==========  Pre Loading   ==========*/
-    setTimeout(function () {
-        $(".preloader").remove();
-    }, 2000);
+	/*==========   Mobile Menu   ==========*/
+	var $navToggler = $('.navbar-toggler');
+	$navToggler.on('click', function () {
+		$(this).toggleClass('actived');
+	});
+	$navToggler.on('click', function () {
+		$('.navbar-collapse').toggleClass('menu-opened');
+	});
 
-    /*==========   Mobile Menu   ==========*/
-    var $navToggler = $('.navbar-toggler');
-    $navToggler.on('click', function () {
-        $(this).toggleClass('actived');
-    })
-    $navToggler.on('click', function () {
-        $('.navbar-collapse').toggleClass('menu-opened');
-    })
+	/*==========   Sticky Navbar   ==========*/
+	$win.on('scroll', function () {
+		if ($win.width() >= 992) {
+			var $navbar = $('.sticky-navbar');
+			if ($win.scrollTop() > 200) {
+				$navbar.addClass('fixed-navbar');
+			} else {
+				$navbar.removeClass('fixed-navbar');
+			}
+		}
+	});
 
-    /*==========   Sticky Navbar   ==========*/
-    $win.on('scroll', function () {
-        if ($win.width() >= 992) {
-            var $navbar = $('.sticky-navbar');
-            if ($win.scrollTop() > 200) {
-                $navbar.addClass('fixed-navbar');
-            } else {
-                $navbar.removeClass('fixed-navbar');
-            }
-        }
-    });
+	/*==========  Search Popup  ==========*/
+	$('.search-popup-trigger').on('click', function (e) {
+		e.preventDefault();
+		$('.search-popup')
+			.toggleClass('active', 'inActive')
+			.removeClass('inActive');
+	});
+	// Close Module Search
+	$('.search__popup-close').on('click', function () {
+		$('.search-popup').removeClass('active').addClass('inActive');
+	});
 
-    /*==========  Search Popup  ==========*/
-    $('.search-popup-trigger').on('click', function (e) {
-        e.preventDefault();
-        $('.search-popup').toggleClass('active', 'inActive').removeClass('inActive');
-    });
-    // Close Module Search
-    $('.search__popup-close').on('click', function () {
-        $('.search-popup').removeClass('active').addClass('inActive');
-    });
+	/*==========   Scroll Top Button   ==========*/
+	var $scrollTopBtn = $('#scrollTopBtn');
+	// Show Scroll Top Button
+	$win.on('scroll', function () {
+		if ($(this).scrollTop() > 700) {
+			$scrollTopBtn.addClass('actived');
+		} else {
+			$scrollTopBtn.removeClass('actived');
+		}
+	});
+	// Animate Body after Clicking on Scroll Top Button
+	$scrollTopBtn.on('click', function () {
+		$('html, body').animate(
+			{
+				scrollTop: 0,
+			},
+			500
+		);
+	});
 
-    /*==========   Scroll Top Button   ==========*/
-    var $scrollTopBtn = $('#scrollTopBtn');
-    // Show Scroll Top Button
-    $win.on('scroll', function () {
-        if ($(this).scrollTop() > 700) {
-            $scrollTopBtn.addClass('actived');
-        } else {
-            $scrollTopBtn.removeClass('actived');
-        }
-    });
-    // Animate Body after Clicking on Scroll Top Button
-    $scrollTopBtn.on('click', function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+	/*==========   Set Background-img to section   ==========*/
+	$('.bg-img').each(function () {
+		var imgSrc = $(this).children('img').attr('src');
+		$(this)
+			.parent()
+			.css({
+				'background-image': 'url(' + imgSrc + ')',
+				'background-size': 'cover',
+				'background-position': 'center',
+			});
+		$(this).parent().addClass('bg-img');
+		if ($(this).hasClass('background-size-auto')) {
+			$(this).parent().addClass('background-size-auto');
+		}
+		$(this).remove();
+	});
 
-    /*==========   Set Background-img to section   ==========*/
-    $('.bg-img').each(function () {
-        var imgSrc = $(this).children('img').attr('src');
-        $(this).parent().css({
-            'background-image': 'url(' + imgSrc + ')',
-            'background-size': 'cover',
-            'background-position': 'center',
-        });
-        $(this).parent().addClass('bg-img');
-        if ($(this).hasClass('background-size-auto')) {
-            $(this).parent().addClass('background-size-auto');
-        }
-        $(this).remove();
-    });
+	/*==========   Add active class to accordions   ==========*/
+	$('.accordion__item-header').on('click', function () {
+		$(this).parent('.accordion-item').addClass('opened');
+		$(this).parent('.accordion-item').siblings().removeClass('opened');
+	});
+	$('.accordion__item-title').on('click', function (e) {
+		e.preventDefault();
+	});
 
-    /*==========   Add active class to accordions   ==========*/
-    $('.accordion__item-header').on('click', function () {
-        $(this).parent('.accordion-item').addClass('opened');
-        $(this).parent('.accordion-item').siblings().removeClass('opened');
-    })
-    $('.accordion__item-title').on('click', function (e) {
-        e.preventDefault()
-    });
+	/*==========   Load More Items  ==========*/
+	function loadMore(loadMoreBtn, loadedItem) {
+		$(loadMoreBtn).on('click', function (e) {
+			e.preventDefault();
+			$(this).fadeOut();
+			$(loadedItem).fadeIn();
+		});
+	}
 
-    /*==========   Load More Items  ==========*/
-    function loadMore(loadMoreBtn, loadedItem) {
-        $(loadMoreBtn).on('click', function (e) {
-            e.preventDefault();
-            $(this).fadeOut();
-            $(loadedItem).fadeIn();
-        })
-    }
+	loadMore('.loadMoreportfolio', '.portfolio-hidden > .portfolio-item');
 
-    loadMore('.loadMoreportfolio', '.portfolio-hidden > .portfolio-item');
+	/*==========   Slick Carousel ==========*/
+	$('.slideshow').slick({
+		autoplay: true,
+		dots: false,
+		adaptiveHeight: true,
+	});
 
-    /*==========   Slick Carousel ==========*/
-    $('.slick-carousel').slick();
+	/*----------  slick Carousel with Filter  ----------*/
+	$('#slick-filter-buttons .nav__link').on('click', function (e) {
+		e.preventDefault();
+		$(this).addClass('active').siblings().removeClass('active');
+		var key = '.' + $(this).data('value');
 
-    $('.slider-with-navs').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        dots: true,
-        asNavFor: '.slider-nav'
-    });
-    $('.slider-nav').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        asNavFor: '.slider-with-navs',
-        dots: false,
-        arrows: false,
-        focusOnSelect: true,
-        centerMode: true,
-        centerPadding: '50px',
-        responsive: [{
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }]
-    });
+		$('#filter-carousel').slick('slickUnfilter');
+		$('#filter-carousel').slick('slickFilter', key).slick('refresh');
+		$('#filter-carousel').slick('slickGoTo', 0);
+	});
 
-    /*----------  slick Carousel with Filter  ----------*/
-    $('#slick-filter-buttons .nav__link').on('click', function (e) {
-        e.preventDefault();
-        $(this).addClass('active').siblings().removeClass('active');
-        var key = "." + $(this).data('value');
+	/*==========  Popup Video  ==========*/
+	$('.popup-video').magnificPopup({
+		mainClass: 'mfp-fade',
+		removalDelay: 0,
+		preloader: false,
+		fixedContentPos: false,
+		type: 'iframe',
+		iframe: {
+			markup:
+				'<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+			patterns: {
+				youtube: {
+					index: 'youtube.com/',
+					id: 'v=',
+					src: '//www.youtube.com/embed/%id%?autoplay=1',
+				},
+			},
+			srcAction: 'iframe_src',
+		},
+	});
+	$('.popup-gallery-item').magnificPopup({
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1],
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		},
+	});
 
-        $('#filter-carousel').slick('slickUnfilter');
-        $('#filter-carousel').slick('slickFilter', key).slick('refresh');
-        $('#filter-carousel').slick('slickGoTo', 0);
-    });
+	/*==========   counterUp  ==========*/
+	$('.counter').counterUp({
+		delay: 10,
+		time: 4000,
+	});
 
+	/*==========  NiceSelect Plugin  ==========*/
+	$('select').niceSelect();
 
-    /*==========  Popup Video  ==========*/
-    $('.popup-video').magnificPopup({
-        mainClass: 'mfp-fade',
-        removalDelay: 0,
-        preloader: false,
-        fixedContentPos: false,
-        type: 'iframe',
-        iframe: {
-            markup: '<div class="mfp-iframe-scaler">' +
-                '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-                '</div>',
-            patterns: {
-                youtube: {
-                    index: 'youtube.com/',
-                    id: 'v=',
-                    src: '//www.youtube.com/embed/%id%?autoplay=1'
-                }
-            },
-            srcAction: 'iframe_src',
-        }
-    });
-    $('.popup-gallery-item').magnificPopup({
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
+	/*==========   portfolio Filtering and Sorting  ==========*/
+	$('#filtered-items-wrap').mixItUp();
+	$('.portfolio-filter li a').on('click', function (e) {
+		e.preventDefault();
+	});
 
-    /*==========   counterUp  ==========*/
-    $(".counter").counterUp({
-        delay: 10,
-        time: 4000
-    });
-
-    /*==========  NiceSelect Plugin  ==========*/
-    $('select').niceSelect();
-
-    /*==========   portfolio Filtering and Sorting  ==========*/
-    $("#filtered-items-wrap").mixItUp();
-    $(".portfolio-filter li a").on("click", function (e) {
-        e.preventDefault();
-    });
-
-    var contactForm = $("#contactForm"),
-        contactResult = $('.contact-result');
-    contactForm.validate({
-        debug: false,
-        submitHandler: function (contactForm) {
-            $(contactResult, contactForm).html('Please Wait...');
-            $.ajax({
-                type: "POST",
-                url: "assets/php/contact.php",
-                data: $(contactForm).serialize(),
-                timeout: 20000,
-                success: function (msg) {
-                    $(contactResult, contactForm).html('<div class="alert alert-success" role="alert"><strong>Thank you. We will contact you shortly.</strong></div>').delay(3000).fadeOut(2000);
-                },
-                error: $('.thanks').show()
-            });
-            return false;
-        }
-    });
+	var contactForm = $('#contactForm'),
+		contactResult = $('.contact-result');
+	contactForm.validate({
+		debug: false,
+		submitHandler: function (contactForm) {
+			$(contactResult, contactForm).html('Please Wait...');
+			$.ajax({
+				type: 'POST',
+				url: 'assets/php/contact.php',
+				data: $(contactForm).serialize(),
+				timeout: 20000,
+				success: function (msg) {
+					$(contactResult, contactForm)
+						.html(
+							'<div class="alert alert-success" role="alert"><strong>Thank you. We will contact you shortly.</strong></div>'
+						)
+						.delay(3000)
+						.fadeOut(2000);
+				},
+				error: $('.thanks').show(),
+			});
+			return false;
+		},
+	});
 });
