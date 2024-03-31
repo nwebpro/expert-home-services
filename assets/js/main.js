a129dd86d131ff9cac7158cc6670cd3b1de86537
nwebpro <abnaeem.bsc@gmail.com>
2024-03-31 11:54:46 +0600

    Delete unused images and icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,23 @@
 /*---------------------------
       Table of Contents
     --------------------
-    01- Pre Loading
-    02- Mobile Menu
-    03- Sticky Navbar
-    04- Search Popup 
-    05- Scroll Top Button
-    06-  Scroll Top Button
-    07- Set Background-img to section 
-    08- Add active class to accordions
-    09- Load More Items
-    10- Slick Carousel
-    11- Popup Video
-    12- CounterUp
-    13- NiceSelect Plugin
-    14- portfolio Filtering and Sorting
+    01- Mobile Menu
+    02- Sticky Navbar
+    03- Scroll Top Button
+    04- Set Background-img to section 
+    05- Slick Carousel
+    06- CounterUp
      
  ----------------------------*/
 
 $(function () {
 	'use strict';
 
-	// Global variables
-	var $win = $(window);
-
-	/*==========  Pre Loading   ==========*/
-	setTimeout(function () {
-		$('.preloader').remove();
-	}, 2000);
+	// Global letiables
+	let $win = $(window);
 
 	/*==========   Mobile Menu   ==========*/
-	var $navToggler = $('.navbar-toggler');
+	let $navToggler = $('.navbar-toggler');
 	$navToggler.on('click', function () {
 		$(this).toggleClass('actived');
 	});
@@ -41,7 +28,7 @@ $(function () {
 	/*==========   Sticky Navbar   ==========*/
 	$win.on('scroll', function () {
 		if ($win.width() >= 992) {
-			var $navbar = $('.sticky-navbar');
+			let $navbar = $('.sticky-navbar');
 			if ($win.scrollTop() > 200) {
 				$navbar.addClass('fixed-navbar');
 			} else {
@@ -50,20 +37,8 @@ $(function () {
 		}
 	});
 
-	/*==========  Search Popup  ==========*/
-	$('.search-popup-trigger').on('click', function (e) {
-		e.preventDefault();
-		$('.search-popup')
-			.toggleClass('active', 'inActive')
-			.removeClass('inActive');
-	});
-	// Close Module Search
-	$('.search__popup-close').on('click', function () {
-		$('.search-popup').removeClass('active').addClass('inActive');
-	});
-
 	/*==========   Scroll Top Button   ==========*/
-	var $scrollTopBtn = $('#scrollTopBtn');
+	let $scrollTopBtn = $('#scrollTopBtn');
 	// Show Scroll Top Button
 	$win.on('scroll', function () {
 		if ($(this).scrollTop() > 700) {
@@ -84,7 +59,7 @@ $(function () {
 
 	/*==========   Set Background-img to section   ==========*/
 	$('.bg-img').each(function () {
-		var imgSrc = $(this).children('img').attr('src');
+		let imgSrc = $(this).children('img').attr('src');
 		$(this)
 			.parent()
 			.css({
@@ -99,118 +74,17 @@ $(function () {
 		$(this).remove();
 	});
 
-	/*==========   Add active class to accordions   ==========*/
-	$('.accordion__item-header').on('click', function () {
-		$(this).parent('.accordion-item').addClass('opened');
-		$(this).parent('.accordion-item').siblings().removeClass('opened');
-	});
-	$('.accordion__item-title').on('click', function (e) {
-		e.preventDefault();
-	});
-
-	/*==========   Load More Items  ==========*/
-	function loadMore(loadMoreBtn, loadedItem) {
-		$(loadMoreBtn).on('click', function (e) {
-			e.preventDefault();
-			$(this).fadeOut();
-			$(loadedItem).fadeIn();
-		});
-	}
-
-	loadMore('.loadMoreportfolio', '.portfolio-hidden > .portfolio-item');
-
 	/*==========   Slick Carousel ==========*/
+	$('.slick-carousel').slick();
 	$('.slideshow').slick({
 		autoplay: true,
 		dots: false,
 		adaptiveHeight: true,
 	});
 
-	/*----------  slick Carousel with Filter  ----------*/
-	$('#slick-filter-buttons .nav__link').on('click', function (e) {
-		e.preventDefault();
-		$(this).addClass('active').siblings().removeClass('active');
-		var key = '.' + $(this).data('value');
-
-		$('#filter-carousel').slick('slickUnfilter');
-		$('#filter-carousel').slick('slickFilter', key).slick('refresh');
-		$('#filter-carousel').slick('slickGoTo', 0);
-	});
-
-	/*==========  Popup Video  ==========*/
-	$('.popup-video').magnificPopup({
-		mainClass: 'mfp-fade',
-		removalDelay: 0,
-		preloader: false,
-		fixedContentPos: false,
-		type: 'iframe',
-		iframe: {
-			markup:
-				'<div class="mfp-iframe-scaler">' +
-				'<div class="mfp-close"></div>' +
-				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-				'</div>',
-			patterns: {
-				youtube: {
-					index: 'youtube.com/',
-					id: 'v=',
-					src: '//www.youtube.com/embed/%id%?autoplay=1',
-				},
-			},
-			srcAction: 'iframe_src',
-		},
-	});
-	$('.popup-gallery-item').magnificPopup({
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0, 1],
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-		},
-	});
-
 	/*==========   counterUp  ==========*/
 	$('.counter').counterUp({
 		delay: 10,
 		time: 4000,
-	});
-
-	/*==========  NiceSelect Plugin  ==========*/
-	$('select').niceSelect();
-
-	/*==========   portfolio Filtering and Sorting  ==========*/
-	$('#filtered-items-wrap').mixItUp();
-	$('.portfolio-filter li a').on('click', function (e) {
-		e.preventDefault();
-	});
-
-	var contactForm = $('#contactForm'),
-		contactResult = $('.contact-result');
-	contactForm.validate({
-		debug: false,
-		submitHandler: function (contactForm) {
-			$(contactResult, contactForm).html('Please Wait...');
-			$.ajax({
-				type: 'POST',
-				url: 'assets/php/contact.php',
-				data: $(contactForm).serialize(),
-				timeout: 20000,
-				success: function (msg) {
-					$(contactResult, contactForm)
-						.html(
-							'<div class="alert alert-success" role="alert"><strong>Thank you. We will contact you shortly.</strong></div>'
-						)
-						.delay(3000)
-						.fadeOut(2000);
-				},
-				error: $('.thanks').show(),
-			});
-			return false;
-		},
 	});
 });
